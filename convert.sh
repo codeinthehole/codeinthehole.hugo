ac29992389eb1719bb39ca8fe54bbf6b42794968
David Winterbottom <david.winterbottom@gmail.com>
2017-03-07 14:00:38 +0000

    Strip image attributes during migrate

--- convert.sh
+++ convert.sh
@@ -4,8 +4,14 @@ INPUT=$1
 
 # Convert sourcecode blocks
 cat $INPUT | \
+    # Convert to recognised directive
     sed 's/.. sourcecode/.. code/' | \
+    # Strip some image attributes which don't convert
     grep -v ":width:" | \
+    grep -v ":class:" | \
+    # RST -> MD
     pandoc --from=rst --to=markdown | \
+    # Strip .sourceCode from codeblocks
     sed 's/{.sourceCode \.\([a-z+]\+\)}/\1/' | \
+    # Inject front-matter
     ./frontmatter.py
