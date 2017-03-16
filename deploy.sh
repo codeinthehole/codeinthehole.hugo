a566252c7e0aa1485d40db1fd9786ecfc75fd42c
David Winterbottom <david.winterbottom@gmail.com>
2017-03-16 23:15:09 +0000

    Tweak deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+# 
+# Deployment script
 
 set -e
+
+# Remove old public folder contents
+rm -rf public/*
 
 # Check working tree is clean
 if ! git diff-index --quiet HEAD --
@@ -13,12 +18,12 @@ SHA=`git log --pretty=format:'%h' -n 1`
 COMMIT_URL="https://github.com/codeinthehole/codeinthehole.hugo/commit/$SHA"
 COMMIT_MSG="Built from $COMMIT_URL"
 
-## Build source into public
+# Build source into public
 hugo
 
 # Commit and push
 cd public
 git add -A
 git commit -m "$COMMIT_MSG"
-git push --set-upstream origin master
+git push origin master
 
